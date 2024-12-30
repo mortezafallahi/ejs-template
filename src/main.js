16dfbbc5239a609000ade3d12a5c8010b9ce934b
morteza <mortezafallahi1385@gmail.com>
2024-12-30 14:37:57 +0330

    add toast

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-import "./styles/index.scss";
+import Toastify from 'toastify-js';
+import 'toastify-js/src/toastify.css';
+import './styles/index.scss';
+
+window.toast = function ({
+  text,
+  type,
+  duration,
+  vertically,
+  className,
+  closeIcon,
+  stopOnFocus,
+  horizontally,
+}) {
+  Toastify({
+    text: text,
+    duration: duration,
+    className: `${type} flex items-center gap-4 ${className}`,
+    newWindow: true,
+    close: closeIcon,
+    gravity: vertically,
+    position: horizontally,
+    stopOnFocus: stopOnFocus,
+  }).showToast();
+};
